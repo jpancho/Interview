fa6ed0f03fe4772ebc3f09d2758669ad46503e79
jpancho <jpancho@my.bcit.ca>
2017-03-03 12:05:24 -0800

    Implement simple FizzBuzz logic

--- src/Interview/FizzBuzz.java
+++ src/Interview/FizzBuzz.java
@@ -1,7 +1,9 @@
 package Interview;
 
 /**
- * FizzBuzz.
+ * FizzBuzz prints "Fizz" if num is a multiple of three
+ * prints "Buzz" if num is a multiple of five
+ * prints "FizzBuzz" if both a multiple of three and five
  *
  * @author Justin
  * @version 1.0.0
@@ -14,6 +16,11 @@ public class FizzBuzz {
      * @param args command line arguments
      */
     public static void main(String[] args) {
-
+        for (int i = 1; i <= 100; i++) {
+            if (i % 3 == 0 && i % 5 == 0) System.out.println(i + " FizzBuzz");
+            else if (i % 3 == 0) System.out.println(i + " Fizz");
+            else if (i % 5 == 0) System.out.println(i + " Buzz");
+            else System.out.println(i + "");
+        }
     }
 }
