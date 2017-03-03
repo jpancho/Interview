63594155f9ad2823b5c488f3e91f391c9d16e15c
jpancho <jpancho@my.bcit.ca>
2017-03-03 11:49:48 -0800

    Initialize FizzBuzz template and javadoc

---
+++ src/Interview/FizzBuzz.java
@@ -0,0 +1,19 @@
+package Interview;
+
+/**
+ * FizzBuzz.
+ *
+ * @author Justin
+ * @version 1.0.0
+ */
+public class FizzBuzz {
+
+    /**
+     * Drives the program.
+     *
+     * @param args command line arguments
+     */
+    public static void main(String[] args) {
+
+    }
+}
